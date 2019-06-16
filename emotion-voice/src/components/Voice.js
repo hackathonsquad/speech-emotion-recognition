1a899245a803b8969d13a460bb900a3045ba21e5
Matt Ye <yefenyi@126.com>
2019-06-16 03:43:08 -0500

    Update Voice.js

--- emotion-voice/src/components/Voice.js
+++ emotion-voice/src/components/Voice.js
@@ -1,15 +1,24 @@
 import { ReactMic } from 'react-mic';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { Doughnut } from 'react-chartjs-2';
 import axios from 'axios';
-
+import * as blobUtil from 'blob-util'
+import PlayIcon from '@material-ui/icons/PlayArrow';
+import ReactAudioPlayer from 'react-audio-player';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
 
 export class Voice extends React.Component {
   constructor(props) {
     super(props);
+    this.onStop = this.onStop.bind(this)
+    this.handleAnalyze = this.handleAnalyze.bind(this);
     this.state = {
       record: false,
       display:false,
@@ -26,7 +35,9 @@ export class Voice extends React.Component {
                 'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 0
-        }]
+        }],
+        audio_set : true,
+        emotion: "none",
         }
     }
 
@@ -47,6 +58,15 @@ export class Voice extends React.Component {
     });
   }
 
+  handleAnalyze(){
+      axios.post('http://localhost:8000/analyse/', {"path":this.state.path}).then(
+          res=>{
+              console.log(res.data)
+              this.setState({emotion:res.data["emotion"]})
+          }
+      )
+  }
+
   stopRecording = () => {
     this.setState({
       record: false,
@@ -58,48 +78,34 @@ export class Voice extends React.Component {
     console.log('chunk of real-time data is: ', recordedBlob);
   }
 
+  setValue(val){
+      this.setState(val)
+  }
+
   onStop(recordedBlob) {
-      const data = new FormData()
+        // const bloburl = recordedBlob.blobURL
+        console.log("sending data...")
+        console.log(recordedBlob)
 
-      var blob = new Blob(["Hello, world!"], { type: 'text/plain' });
-      var blobUrl = URL.createObjectURL(blob);
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', recordedBlob.blobURL, true);
+        xhr.responseType = 'blob';
+        const scope = this
+        xhr.onload = function(e) {
+          if (this.status == 200) {
+            var myBlob = this.response;
+            let blobData = new FormData();
+            blobData.append('file', myBlob);
 
-      console.log(blobUrl)
-
-      blobUrl = data['blobURL']
-      var xhr = new XMLHttpRequest;
-      xhr.responseType = 'blob';
-
-      xhr.onload = function() {
-           var recoveredBlob = xhr.response;
-
-           var reader = new FileReader;
-
-           reader.onload = function() {
-             var blobAsDataUrl = reader.result;
-             window.location = blobAsDataUrl;
-           };
-
-           reader.readAsDataURL(recoveredBlob);
-           console.log(reader)
-
-           axios.post('http://localhost:8000/record/', reader, {
-               headers: {
-                   'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                 },}).then(
-               res=>{
-                   console.log(res.data)
-                   //this.setState({path:res.data["dir"], audio_set: true});
-               }
-           )
-      };
-
-      xhr.open('GET', blobUrl);
-      console.log("send")
-      xhr.send();
-
-
-    //console.log('recordedBlob is: ', recordedBlob);
+            axios.post('http://localhost:8000/record/', blobData, {}).then(
+                res=>{
+                    console.log(res.data)
+                    scope.setState({path:res.data["dir"], audio_set: true})
+                })
+          }
+        };
+        xhr.send();
+        console.log("sended data...")
   }
 
   render() {
@@ -133,27 +139,71 @@ export class Voice extends React.Component {
                 <Button onClick={this.stopRecording} disabled={!this.state.record} color="secondary" type="button">Stop</Button>
         </Grid>
 
-        <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
 
-          {this.state.display&&
-               <Doughnut
-                 data={this.state.data}
-                 />
-           }
+       {this.state.audio_set &&
+           <div>
+
+                 <div onClick = {() => {this.handleAnalyze()}}>
+                     <div style={{textAlign:"center", margin:"20px"}}>
+                       <Fab  aria-label="Delete" >
+                         <PlayIcon />
+                       </Fab>
+                      </div>
+                      <audio ref={(audio) => { this.audio = audio; }}
+                      src="src/components/test.wav">
+                      </audio>
+                 </div>
+
+
+
+   <div>
+     <Grid container spacing={3}>
+       <Grid item xs={12}  style = {{
+           textAlign: 'center',
+         }}>
+         <Paper  style={{
+             backgroundColor:(this.state.emotion=="happy")?"#f50057":"grey",
+             color:(this.state.emotion=="happy")?"white":"black",
+         }}>Happy</Paper>
+       </Grid>
+       <Grid item xs={12} style = {{
+           textAlign: 'center',
+         }}>
+         <Paper  style={{
+             backgroundColor:(this.state.emotion=="sad")?"#f50057":"grey",
+             color:(this.state.emotion=="sad")?"white":"black",
+         }}>Sad</Paper>
+       </Grid>
+       <Grid item xs={12} style = {{
+           textAlign: 'center',
+         }}>
+         <Paper  style={{
+             backgroundColor:(this.state.emotion=="neutral")?"#f50057":"grey",
+             color:(this.state.emotion=="neutral")?"white":"black",
+         }}>Neutral</Paper>
+       </Grid>
+       <Grid item xs={12} style = {{
+           textAlign: 'center',
+         }}>
+         <Paper  style={{
+             backgroundColor:(this.state.emotion=="fear")?"#f50057":"grey",
+             color:(this.state.emotion=="fear")?"white":"black",
+         }}>Fear</Paper>
+       </Grid>
+       <Grid item xs={12} style = {{
+           textAlign: 'center',
+         }}>
+         <Paper  style={{
+             backgroundColor:(this.state.emotion=="angry")?"#f50057":"grey",
+             color:(this.state.emotion=="angry")?"white":"black",
+         }}>Angry</Paper>
        </Grid>
 
-       <Grid
-           container
-           direction="row"
-           justify="center"
-           alignItems="center"
-       >
-       </Grid>
+     </Grid>
+   </div>
+</div>
+
+}
       </div>
     );
   }
